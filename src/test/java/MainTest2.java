aec74e2b96e02ae2be300d38e470f9c553c17c63
曾鸿发 <zenghongfa@sundear.com>
2021-12-29 20:11:29 +0800

    [add] binary search
    [add] binary search

---
+++ src/test/java/MainTest2.java
@@ -0,0 +1,15 @@
+/**
+ * @author: 曾鸿发
+ * @create: 2021-12-14 20:16
+ * @description：测试类程序
+ **/
+public class MainTest2 {
+
+    private void test1(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/ShowMeBug.java
@@ -0,0 +1,23 @@
+/**
+ * @author: 曾鸿发
+ * @create: 2021-12-14 20:43
+ * @description：
+ **/
+class Solution {
+    // 请按你的实际需求修改参数
+    public int binarySearch(int[] arr, int target) {
+        // 在这⾥书写你的代码
+        if(arr.length == 0)
+        return -1;
+    }
+}
+
+public class ShowMeBug {
+    public static void main(String[] args) {
+        Solution instance = new Solution();
+        int[] arr = {0, 1, 3, 5, 7, 10};
+        int target = 3;
+        int ret = instance.binarySearch(arr, target);
+        System.out.println(ret);
+    }
+}
